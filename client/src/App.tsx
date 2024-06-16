8331a82df45eb9ae3390df712c52865970d422e7
deepanshu20101 <deepanshusushil@gmail.com>
2024-06-16 17:50:14 +0530

    Login page UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import "./App.css";
+import LoginPage from "./components/login";
 
 function App() {
-  return <div className="App">Client</div>;
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/login.tsx
@@ -0,0 +1,92 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Link,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+
+const LoginPage = () => {
+  return (
+    <Grid container sx={{ height: "100vh" }}>
+      <Grid item lg={7}>
+        <img
+          src="/assets/2.png"
+          alt="image"
+          style={{ width: "100%", height: "100%", display: "block" }}
+        />
+      </Grid>
+      <Grid item lg={5} component={Paper} elevation={6}>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h5">Sign in</Typography>
+          <Box component="form" sx={{ mt: 1 }}>
+            <TextField
+              required
+              margin="normal"
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              type="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              fullWidth
+              margin="normal"
+              required
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default LoginPage;
