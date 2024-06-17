98cbd46f152523b191069df135f9c60a5c1060d2
deepanshu20101 <deepanshusushil@gmail.com>
2024-06-17 22:47:50 +0530

    added sign up page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import LoginPage from "./components/login";
+import LoginPage from "./components/auth/login";
+import SignUp from "./components/auth/signUp";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./components/home/home";
 
 function App() {
   return (
     <>
-      <LoginPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ client/src/components/auth/signUp.tsx
@@ -0,0 +1,109 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Link,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+
+const SignUp = () => {
+  return (
+    <Grid container sx={{ height: "100vh" }}>
+      <Grid item lg={7}>
+        <img
+          src="/assets/2.png"
+          alt="image"
+          style={{ width: "100%", height: "100%", display: "block" }}
+        />
+      </Grid>
+      <Grid item lg={5} component={Paper} elevation={6}>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h5">Sign Up</Typography>
+          <Box component="form" sx={{ mt: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  margin="normal"
+                  autoComplete="firstName"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lastName"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SignUp;
