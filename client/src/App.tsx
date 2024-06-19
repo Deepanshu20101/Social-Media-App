2b6ab05c4db0f4d9f927c05b93cf5883142cc404
deepanshu20101 <deepanshusushil@gmail.com>
2024-06-19 15:33:30 +0530

    updated topbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,13 @@ import LoginPage from "./components/auth/login";
 import SignUp from "./components/auth/signUp";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/home/home";
+import TopBar from "./components/topbar/topbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <TopBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
