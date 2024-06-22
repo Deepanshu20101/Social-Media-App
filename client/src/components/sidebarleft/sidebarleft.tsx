9d425e5e1e93d170fa39473c795fb9b13a7f09cb
deepanshu20101 <deepanshusushil@gmail.com>
2024-06-22 12:03:27 +0530

    sidebar changes

--- client/src/components/sidebarleft/sidebarleft.tsx
+++ client/src/components/sidebarleft/sidebarleft.tsx
@@ -1,4 +1,27 @@
-import { Drawer, Toolbar } from "@mui/material";
+import {
+  Bookmark,
+  Chat,
+  Event,
+  Group,
+  Help,
+  PlayCircle,
+  RssFeed,
+  School,
+  Work,
+} from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+} from "@mui/material";
 
 const SideBarLeft = () => {
   return (
@@ -12,7 +35,101 @@ const SideBarLeft = () => {
       }}
     >
       <Toolbar />
-      <div>drawer left</div>
+      <Box sx={{ overflow: "auto" }}>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <RssFeed />
+              </ListItemIcon>
+              <ListItemText primary="Feed" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Chat />
+              </ListItemIcon>
+              <ListItemText primary="Chats" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <PlayCircle />
+              </ListItemIcon>
+              <ListItemText primary="Videos" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Group />
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Bookmark />
+              </ListItemIcon>
+              <ListItemText primary="Bookmarks" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Help />
+              </ListItemIcon>
+              <ListItemText primary="Questions" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Work />
+              </ListItemIcon>
+              <ListItemText primary="Jobs" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Event />
+              </ListItemIcon>
+              <ListItemText primary="Events" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <School />
+              </ListItemIcon>
+              <ListItemText primary="Courses" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemAvatar>
+                <Avatar />
+              </ListItemAvatar>
+              <ListItemText primary="Luffy" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemAvatar>
+                <Avatar />
+              </ListItemAvatar>
+              <ListItemText primary="Zoro" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
     </Drawer>
   );
 };
--- client/src/components/sidebarright/sidebarright.tsx
+++ client/src/components/sidebarright/sidebarright.tsx
@@ -1,4 +1,19 @@
-import { Drawer, Toolbar } from "@mui/material";
+import { MoreHoriz, Search } from "@mui/icons-material";
+import { Badge } from "@mui/joy";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
 const SideBarRight = () => {
   return (
@@ -6,13 +21,87 @@ const SideBarRight = () => {
       variant="permanent"
       anchor="right"
       sx={{
-        width: 240,
+        width: 340,
         flexShrink: 0,
-        [`& .MuiDrawer-paper`]: { width: 240, boxSizing: "border-box" },
+        [`& .MuiDrawer-paper`]: { width: 340, boxSizing: "border-box" },
       }}
     >
       <Toolbar />
-      <div>drawer right</div>
+      <Box sx={{ overflow: "auto" }}>
+        <Box sx={{ display: "flex", p: 2, alignItems: "center" }}>
+          <img
+            src="/assets/giftbox.png"
+            style={{ width: "40px", height: "40px" }}
+          />
+          <Typography variant="body1" sx={{ ml: 2 }}>
+            <b>Usopp</b> and <b>2 other friends</b> have their birthdays today.
+          </Typography>
+        </Box>
+        <Divider />
+        <Box sx={{ display: "flex", px: 2, pt: 2 }}>
+          <Typography variant="h6" sx={{ mr: "auto" }}>
+            Contacts
+          </Typography>
+          <IconButton>
+            <Search />
+          </IconButton>
+          <IconButton>
+            <MoreHoriz />
+          </IconButton>
+        </Box>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemAvatar>
+                <Badge badgeInset="14%" color="success">
+                  <Avatar />
+                </Badge>
+              </ListItemAvatar>
+              <ListItemText primary="Chopper" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemAvatar>
+                <Badge badgeInset="14%" color="success">
+                  <Avatar />
+                </Badge>
+              </ListItemAvatar>
+              <ListItemText primary="Nami" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemAvatar>
+                <Badge badgeInset="14%" color="success">
+                  <Avatar />
+                </Badge>
+              </ListItemAvatar>
+              <ListItemText primary="Sanji" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemAvatar>
+                <Badge badgeInset="14%" color="success">
+                  <Avatar />
+                </Badge>
+              </ListItemAvatar>
+              <ListItemText primary="Franky" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemAvatar>
+                <Badge badgeInset="14%" color="success">
+                  <Avatar />
+                </Badge>
+              </ListItemAvatar>
+              <ListItemText primary="Brook" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
     </Drawer>
   );
 };
